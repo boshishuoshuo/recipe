47f9426ec6edd0fa3341e0a5774985b5226c2ad1
Yan Feng <fengyan0121@gmail.com>
2020-11-23 13:50:25 -0500

    Add recipe list containing name, description, and img-link.

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,11 +1,19 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Recipe } from '../recipe.model';
+
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe('A Test Recipe', 'This is a test', 
+    'https://storage.needpix.com/rsynced_images/food-1459693_1280.jpg'),
+    new Recipe('A Test Recipe', 'This is a test', 
+    'https://storage.needpix.com/rsynced_images/food-1459693_1280.jpg')
+  ];
 
   constructor() { }
 
